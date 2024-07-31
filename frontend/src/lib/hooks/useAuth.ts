97ffb3247700791fc20afeeb8e4ca5fbfd8c22f8
Lilit Yengoian <yengoian@usc.edu>
2024-07-31 11:39:19 -0700

    adding env variable, changing port, using firebase config in useauth

--- frontend/src/lib/hooks/useAuth.ts
+++ frontend/src/lib/hooks/useAuth.ts
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged, signOut, User } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
 
 const useAuth = () => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user ? user : null);
     });
